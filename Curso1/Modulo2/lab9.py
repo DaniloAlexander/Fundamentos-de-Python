d64a99c06866f5d1830436f9f9151e8aa8fb48fe
Danilo <daniloalexanderv@gmail.com>
2025-11-08 02:13:58 -0600

    while

---
+++ Curso1/Modulo2/lab9.py
@@ -0,0 +1,19 @@
+secret_number = 777
+number_user = int(input("Introduce tu número aquí: "))
+
+print(
+"""
++================================+
+| ¡Bienvenido a mi juego, muggle!|
+| Introduce un número entero     |
+| y adivina qué número he        |
+| elegido para ti.               |
+|¿Cuál es el número secreto?     |
++================================+
+""")
+
+while secret_number != number_user:
+    print("¡Ja, ja! ¡Estás atrapado en mi bucle!")
+    number_user = int(input("Introduce tu número aquí: "))
+
+print("¡Bien hecho, muggle! Eres libre ahora.")
