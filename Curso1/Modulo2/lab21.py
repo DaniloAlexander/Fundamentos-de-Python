35ff0456db9ccdb77dab600a931a5309afbd1ee0
Danilo <daniloalexanderv@gmail.com>
2025-11-12 15:37:18 -0600

    actualización

---
+++ Curso1/Modulo2/lab21.py
@@ -0,0 +1,9 @@
+lista_conejo = [1,2,3,4,5]
+# Paso 1: escribe una línea de código que solicite al usuario
+# reemplazar el número de en medio con un número entero ingresado por el usuario.
+lista_conejo[2] = int(input("Ingrese un número entero: "))
+# Paso 2: escribe aquí una línea de código que elimine el último elemento de la lista.
+del lista_conejo[4]
+# Paso 3: escribe aquí una línea de código que imprima la longitud de la lista existente.
+print(len(lista_conejo))
+print(lista_conejo)
---
+++ Curso1/Modulo2/lab23.py
@@ -0,0 +1,6 @@
+list = []
+
+for number in range(1,6):
+    number = int(input("Ingrese un entero: "))
+    list.insert(0,number)
+print(list)
